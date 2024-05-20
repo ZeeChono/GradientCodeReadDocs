4af7a8b9a5ea695edd37dca650ffe1f83d481038
Zichen Chang <54852475+ZeeChono@users.noreply.github.com>
2024-05-20 15:43:12 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'AWS Gradient Coding Sim'
+copyright = '2024, Zichen'
+author = 'Zichen'
 
 release = '0.1'
 version = '0.1.0'
